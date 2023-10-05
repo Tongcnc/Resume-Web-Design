5149388289316d9db304009e29b914c70bf3bd82
Tongcnc <tongcnc3@gmail.com>
2023-10-05 13:28:19 +0700

    fix: change Layout Skill

--- Page/mySkillPage.jsx
+++ Page/mySkillPage.jsx
@@ -4,22 +4,30 @@ import { mySkillInfo } from "../Info/allInfo";
 function MySkillPage() {
   return (
     <div className="skill-container" id="skill">
-      <h2 className="skill-title">Additional passions</h2>
+      <div className="skill-text-box">
+        <p>My abilities</p>
+        <h2 className="skill-title">Experience</h2>
+      </div>
       <div className="skill-list">
         {mySkillInfo.map((item) => {
           return (
             <div key={item.id} className="skill">
               <div className="skill-title-box">
-                <img
+                {/* <img
                   src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/7.5.0/png/iconmonstr-3d-lined.png&r=255&g=255&b=255"
                   alt="check icon"
                   width="50px"
-                />
+                /> */}
                 <h2>{item.title}</h2>
               </div>
               <ul>
                 {item.program.map((skill, index) => (
                   <li key={index} data-aos="fade-up" data-aos-duration="1500">
+                    <img
+                      src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/7.3.0/png/iconmonstr-cube-filled.png&r=201&g=97&b=222"
+                      alt="check icon"
+                      width="30px"
+                    />
                     {skill}
                   </li>
                 ))}
