c097b43387ead8def1c57fedba1a20656f21b9b9
Tongcnc <tongcnc3@gmail.com>
2023-10-09 17:26:47 +0700

    fix: popup close button

--- components/PopUp.jsx
+++ components/PopUp.jsx
@@ -18,13 +18,18 @@ function PopUp({
         data-aos-duration="500"
       >
         <div className="popup-button">
-          <button onClick={() => setShowPopup(false)} className="buttonX">
-            <img
-              src="https://iconmonstr.com/wp-content/g/gd/makefg.php?i=../releases/preview/7.2.0/png/iconmonstr-x-mark-circle-filled.png&r=255&g=255&b=255"
-              alt="cancel button"
-              width="30px"
-            />
-          </button>
+          <svg
+            width="30px"
+            fill="currentColor"
+            strokeLinejoin="round"
+            strokeMiterlimit="2"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            onClick={() => setShowPopup(false)}
+            className="buttonX"
+          >
+            <path d="m12 10.93 5.719-5.72c.146-.146.339-.219.531-.219.404 0 .75.324.75.749 0 .193-.073.385-.219.532l-5.72 5.719 5.719 5.719c.147.147.22.339.22.531 0 .427-.349.75-.75.75-.192 0-.385-.073-.531-.219l-5.719-5.719-5.719 5.719c-.146.146-.339.219-.531.219-.401 0-.75-.323-.75-.75 0-.192.073-.384.22-.531l5.719-5.719-5.72-5.719c-.146-.147-.219-.339-.219-.532 0-.425.346-.749.75-.749.192 0 .385.073.531.219z" />
+          </svg>
         </div>
         <div className="popup-container">
           <div className="popup-content">
